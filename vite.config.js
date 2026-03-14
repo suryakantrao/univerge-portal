aa93e6370da14e5e8a919a53236fadaa39454020
suryakantrao <suryakant@plaxonic.com>
2026-03-14 13:01:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   build: {
     outDir: 'dist',
     sourcemap: false,
@@ -23,6 +17,5 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    open: true,
   },
 })
